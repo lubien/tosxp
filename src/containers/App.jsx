eb7327ee1fae6d90dc8f986e544079e204c955d0
lubien <lubien1996@gmail.com>
2016-04-21 18:49:57 -0300

    Add floating button

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,11 +12,28 @@ injectTapEventPlugin();
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ActionCode from 'material-ui/svg-icons/action/code';
+
+const floatingButtonStyle = {
+  position: 'fixed',
+  bottom: 24,
+  right: 12,
+};
+
 const App = () => (
   <MuiThemeProvider muiTheme={getMuiTheme()}>
     <section>
       <Navbar />
       <Content />
+      <FloatingActionButton
+        href="https://github.com/lubien/tosxp"
+        target="_blank"
+        style={floatingButtonStyle}
+        linkButton
+      >
+        <ActionCode />
+      </FloatingActionButton>
     </section>
   </MuiThemeProvider>
 );
