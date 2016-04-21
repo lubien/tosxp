93357ef7f49fa740cd6fe7040dde54d1babba736
lubien <lubien1996@gmail.com>
2016-04-21 01:42:22 -0300

    Reconfigure webpack
    * Fix typo at lie 9
    * Fix output.publicPath
    * Separate .js and .jsx so .jsx can be reloaded with react-hot loader
    * Configure CSS loader for flexboxgrid and JSON for src/data/*.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,44 @@ const dist = 'dist/';
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000/',
-    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
     './src/index',
   ],
   output: {
     path: path.join(__dirname, dist),
-    publicPath: dist,
+    publicPath: 'http://localhost:3000/',
     contentBase: dist,
     filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        loaders: ['react-hot', 'babel-loader'],
         exclude: /node_modules/,
       },
       {
         test: /\.s(a|c)ss$/,
         loaders: ['style', 'css', 'sass'],
       },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
     ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules'],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
