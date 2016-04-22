b905af2ef66f82736bdbefc94f764ea466a6ab27
lubien <lubien1996@gmail.com>
2016-04-22 10:29:20 -0300

    Check if window is defined

--- src/reducers/sidebar.js
+++ src/reducers/sidebar.js
@@ -1,7 +1,8 @@
 import { TOGGLE_SIDEBAR } from '../constants/sidebar';
 
 export const DOCKED_MIN_WIDTH = 980;
-export const INITIAL_STATE = window.innerWidth >= DOCKED_MIN_WIDTH;
+export const INITIAL_STATE = typeof window !== 'undefined' &&
+ window.innerWidth >= DOCKED_MIN_WIDTH;
 
 export default function sidebarReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
